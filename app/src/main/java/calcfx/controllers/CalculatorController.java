dc9ff3af52a37472a6260ae32380f860d70e2606
Kingkor Roy Tirtho <krtirtho@gmail.com>
2024-09-23 09:59:44 +0600

    chore: doc comments

--- app/src/main/java/calcfx/controllers/CalculatorController.java
+++ app/src/main/java/calcfx/controllers/CalculatorController.java
@@ -15,7 +15,6 @@ public class CalculatorController {
 
     private final ArrayList<String> operators = new ArrayList<>(Arrays.asList("+", "-", "*", "/"));
 
-
     public void onClickInputButton(ActionEvent event) {
         String buttonText = ((javafx.scene.control.Button) event.getSource()).getText();
         display.appendText(buttonText);
@@ -27,8 +26,9 @@ public class CalculatorController {
         String[] operatorArr = new String[operators.size()];
         operators.toArray(operatorArr);
         boolean endsWithOp = Arrays.stream(operatorArr).anyMatch(text::endsWith);
+        boolean endsWithPeriod = text.endsWith(".");
 
-        if (text.isEmpty() || endsWithOp) {
+        if (text.isEmpty() || endsWithOp || endsWithPeriod) {
             return;
         }
         display.appendText(operator);
@@ -62,31 +62,43 @@ public class CalculatorController {
     private String evaluate(String expression) {
         List<String> tokens = new ArrayList<>();
         int i = 0;
+
+        /**
+         * Evaluating the multiplication and division operators first and saving
+         * their results in the tokens
+         *
+         * Then, iterating over all the tokens only for addition and subtraction
+         */
         while (i < expression.length()) {
-            StringBuilder sb = new StringBuilder();
-            if (Character.isDigit(expression.charAt(i)) || expression.charAt(i) == '.') {
-                while (i < expression.length() && (Character.isDigit(expression.charAt(i)) || expression.charAt(i) == '.')) {
-                    sb.append(expression.charAt(i));
+            StringBuilder subExpression = new StringBuilder();
+            boolean isCurrentCharNumber = Character.isDigit(expression.charAt(i));
+            boolean isCurrentCharPeriod = expression.charAt(i) == '.';
+
+            if (isCurrentCharNumber || isCurrentCharPeriod) {
+                while (i < expression.length() && (isCurrentCharNumber || isCurrentCharPeriod)) {
+                    subExpression.append(expression.charAt(i));
                     i++;
                 }
-                tokens.add(sb.toString());
-            } 
-            else {
+                tokens.add(subExpression.toString());
+            } else {
                 char operator = expression.charAt(i);
                 if (operator == '*' || operator == '/') {
-                    double num1 = Double.parseDouble(tokens.remove(tokens.size() - 1));
-                    i++; 
-                    sb.setLength(0);
-                    while (i < expression.length() && (Character.isDigit(expression.charAt(i)) || expression.charAt(i) == '.')) {
-                        sb.append(expression.charAt(i));
+                    double lastNum = Double.parseDouble(tokens.remove(tokens.size() - 1));
+                    i++;
+                    subExpression.setLength(0);
+                    while (
+                        i < expression.length() && 
+                        (Character.isDigit(expression.charAt(i)) || expression.charAt(i) == '.')
+                    ) {
+                        subExpression.append(expression.charAt(i));
                         i++;
                     }
-                    double num2 = Double.parseDouble(sb.toString());
-                    
+                    double subExpressionNum = Double.parseDouble(subExpression.toString());
+
                     if (operator == '*') {
-                        tokens.add(String.valueOf(num1 * num2));
+                        tokens.add(String.valueOf(lastNum * subExpressionNum));
                     } else {
-                        tokens.add(String.valueOf(num1 / num2));
+                        tokens.add(String.valueOf(lastNum / subExpressionNum));
                     }
                 } else {
                     tokens.add(String.valueOf(operator));
@@ -100,18 +112,17 @@ public class CalculatorController {
         while (i < tokens.size()) {
             String operator = tokens.get(i);
             double nextNum = Double.parseDouble(tokens.get(i + 1));
-            
+
             if (operator.equals("+")) {
                 result += nextNum;
             } else if (operator.equals("-")) {
                 result -= nextNum;
             }
-            i += 2; 
+            i += 2;
         }
 
         return String.valueOf(result);
     }
-
 
     public void onClickEqual(ActionEvent event) {
         String text = display.getText();
